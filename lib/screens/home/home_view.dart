ccc341177668070036673524e14f2848f594e703
Adrees Nazir <adreesking975@gmail.com>
2024-08-01 04:22:32 -0700

    Mainy is code main responsiveness ka issue resolve ker diya hai

--- lib/screens/home/home_view.dart
+++ lib/screens/home/home_view.dart
@@ -7,7 +7,7 @@ import 'home_logic.dart';
 
 class HomePage extends StatelessWidget {
   final logic = Get.put(HomeLogic());
-
+ ///I have created my own branch
   @override
   Widget build(BuildContext context) {
     return Column(
